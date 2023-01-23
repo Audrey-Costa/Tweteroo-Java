4975b88af74b6921e4acdc564e2ff010a8f7526e
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-01-23 09:11:06 -0300

    Add Tweet Controller Methods

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -4,21 +4,33 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.model.TweetModel;
+import com.tweteroo.api.service.TweetService;
 
 @RestController
 @RequestMapping
 public class TweetController {
 
   @Autowired
-  private service;
+  private TweetService service;
 
-  @GetMapping
-  public List<TweetModel> listTweetsPage(){
+  @GetMapping("/tweets")
+  public List<TweetModel> listTweetsPage() {
     
+  }
+
+  @GetMapping("/tweets")
+  public List<TweetModel> listUserTweets() {
+
+  }
+
+  @PostMapping("/tweets")
+  public void postTweet() {
+
   }
 
 }
