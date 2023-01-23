a35e6a6955f684d035044af6dbea732f11b2ac61
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-01-23 06:26:10 -0300

    Create Tweet Repository

--- src/main/java/com/tweteroo/api/controller/TweetController.java
+++ src/main/java/com/tweteroo/api/controller/TweetController.java
@@ -1,16 +1,23 @@
 package com.tweteroo.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tweteroo.api.model.TweetModel;
+
 @RestController
 @RequestMapping
 public class TweetController {
-  
+
+  @Autowired
+  private service;
 
   @GetMapping
-  public List<> listTweetsPage(){
+  public List<TweetModel> listTweetsPage(){
     
   }
 
