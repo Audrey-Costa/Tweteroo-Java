bcc8548986a648ab7583fe4fcf3d1a5756c1d6d0
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-01-23 05:54:43 -0300

    Create User Model

--- src/main/java/com/tweteroo/api/model/UserModel.java
+++ src/main/java/com/tweteroo/api/model/UserModel.java
@@ -1,5 +1,31 @@
 package com.tweteroo.api.model;
 
+import com.tweteroo.api.DTO.UserDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class UserModel {
-  
+  public UserModel(UserDTO data){
+    this.username = data.username();
+    this.avatar = data.avatar();
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column
+  private String username;
+
+  @Column
+  private String avatar;
 }
