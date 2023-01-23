c64e058fc1280053ffa805c0ddb84df455f6f82f
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-01-23 09:14:58 -0300

    Add User Controller Method

--- src/main/java/com/tweteroo/api/controller/UserController.java
+++ src/main/java/com/tweteroo/api/controller/UserController.java
@@ -1,5 +1,21 @@
 package com.tweteroo.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweteroo.api.service.UserService;
+
+@RestController
+@RequestMapping
 public class UserController {
   
+  @Autowired
+  private UserService service;
+
+  @PostMapping("/sign-up")
+  public void signUp() {
+    
+  }
 }
